f177fdf07eb7b12cf74e84238620e0da04978131
Jarod <mordredbta3011@gmail.com>
2022-08-31 16:47:25 +0200

    Eval_Fini

--- Eval/Exo_5.js
+++ Eval/Exo_5.js
@@ -91,29 +91,48 @@
 // };
 
 
-//Nom
-
-
+//Appelle de function
 let bouton_envoyer = document.getElementById("Envoyer");
 bouton_envoyer.addEventListener("click", verif_nom);
+bouton_envoyer.addEventListener("click", verif_prenom);
+bouton_envoyer.addEventListener("click", verif_ville);
+bouton_envoyer.addEventListener("click", verif_adress);
+bouton_envoyer.addEventListener("click", verif_codepostal);
+bouton_envoyer.addEventListener("click", verif_DatedeN);
+bouton_envoyer.addEventListener("click", verif_Email);
+bouton_envoyer.addEventListener("click", verif_HF);
+bouton_envoyer.addEventListener("click", verif_select);
+bouton_envoyer.addEventListener("click", verif_question);
+bouton_envoyer.addEventListener("click", verif_checkbox);
 
+//Filtre
 let resultat
 let filtreN = new RegExp("^[A-Z]+[a-z]+$");
+let filtreP = new RegExp("^[A-Z]+[a-z]+$");
+let filtreV = new RegExp("^[A-Z]+[a-z]+$");
+let filtreA = new RegExp("^[0-9]+.[A-Za-z]+.[A-Za-z]+.[A-Za-z]+$");
+let filtreC = new RegExp("^[0-9]+$");
+let filtreD = new RegExp("^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$");
+let filtreE = new RegExp("^[a-z0-9]+[.]?[a-z0-9]*@[a-z]+[.][a-z]{2,4}$");
+let filtreQ = new RegExp("^.+");
 
+
+//Nom
 function verif_nom(n){
 
     let N = document.getElementById("nom");
 
     if(N == 'null'){
         filtreN;
-        alert("Rentre un nom");
+        // alert("Rentre un nom");
         n.preventDefault()
     }else{
         filtreN;
         resultat = filtreN.test(N.value);
     }if(resultat == false){
 
-        alert("Arrete tu t'appelle pas comme ça...");
+        document.getElementById('1').innerHTML="Arrete tu t'appelle pas comme ça XD";
+        // alert("Arrete tu t'appelle pas comme ça...");
         n.preventDefault();
     }
 }
@@ -121,24 +140,205 @@ verif_nom;
 
 //Prénom
 
-
-let filtreP = new RegExp("^[A-Z]+[a-z]+$");
-
 function verif_prenom(p){
 
     let P = document.getElementById("prenom");
 
-    if(P == " "){
+    if(P == "null"){
         filtreP;
-        alert("Rentre un prenom");
+        // alert("Rentre un prenom");
         p.preventDefault()
     }else{
         filtreP;
         resultat = filtreP.test(P.value);
     }if(resultat == false){
 
-        alert("Arrete ces pas ton prénom...");
+        document.getElementById('2').innerHTML="Arrete ces pas ton prénom mdr";
+        // alert("Arrete ces pas ton prénom...");
         p.preventDefault();
     }
 }
 verif_prenom;
+
+//Ville
+
+function verif_ville(v){
+
+    let V = document.getElementById("Ville");
+
+    if(V == "null"){
+        filtreV;
+        // alert("Aller champion rentre un ville");
+        v.preventDefault()
+    }else{
+        filtreV;
+        resultat = filtreV.test(V.value);
+    }if(resultat == false){
+
+        document.getElementById('7').innerHTML="Elle peux pas s'appeler comme ça XD";
+        // alert("La ville bro");
+        v.preventDefault();
+    }
+}
+verif_ville;
+
+
+//Adress
+
+function verif_adress(a){
+
+    let A = document.getElementById("Adress");
+
+    if(A == "null"){
+        filtreA;
+        // alert("Rentre une adress bro");
+        a.preventDefault();
+    }else{
+        filtreA;
+        resultat = filtreA.test(A.value);
+    }if(resultat == false){
+
+        document.getElementById('6').innerHTML="Ba t'es sur qu'elle est bonne ton adresse ?";
+        // alert("Ba t'es sur qu'elle est bonne ton adresse ? ");
+        a.preventDefault();
+    }
+}
+verif_adress;
+
+
+//CodePostal
+
+function verif_codepostal(c){
+
+    let C = document.getElementById("CodePostal");
+
+    if(C == "null"){
+        filtreC;
+        // alert("Rentre un code postal");
+        c.preventDefault();
+    }else{
+        filtreC;
+        resultat = filtreC.test(C.value);
+    }if(resultat == false){
+
+        document.getElementById('5').innerHTML="Met un code bon frerot";
+        // alert("Met un code postal bon frerot");
+        c.preventDefault();
+    }
+}
+verif_codepostal;
+
+
+//Date de Naissance
+
+function verif_DatedeN(d){
+
+    let D = document.getElementById("date");
+    // console.log(D)                               //Test pour savoir comment la date s'affiche
+
+    if(D == "null"){
+        filtreD;
+        // alert("Rentre ta date de naissance");
+        d.preventDefault();
+    }else{
+        filtreD;
+        resultat = filtreD.test(D.value);
+    }if(resultat == false){
+
+        document.getElementById('4').innerHTML="Met la date de naissance bro";
+        // alert("Met la date de naissance bro");
+        d.preventDefault();
+    }
+}
+verif_codepostal;
+
+
+//Email
+
+function verif_Email(e){
+
+    let E = document.getElementById("Email");
+
+    if(E == "null"){
+        filtreE;
+        // alert("Le mail stp");
+        e.preventDefault();
+    }else{
+        filtreE;
+        resultat = filtreE.test(E.value);
+    }if(resultat == false){
+
+        document.getElementById('8').innerHTML="Met le mail mec";
+        // alert("Met bien ton mail mec");
+        e.preventDefault();
+    }
+}
+verif_Email;
+
+
+//Sexe Homme/Femme
+
+function verif_HF(hf){
+
+    if(document.getElementById("H").checked){
+        // alert ("ces bon");
+            
+    }else if(document.getElementById("F").checked){
+            // alert ('tu es bon Jarod')
+    }else {
+        document.getElementById('3').innerHTML="Faut indiquer ton genre toi";
+        // alert("Tu a oublier d'indiquer ton genre")
+    }
+};
+verif_HF;
+
+
+//Select
+
+function verif_select(s){
+    if(document.getElementById("couleur").value == ""){
+
+        document.getElementById('9').innerHTML="Faut selectionner ce que tu veux la";
+        // alert("Tu na rien selectionner dans le Sujet");
+        s.preventDefault();
+    }
+}
+
+verif_select;
+
+
+//Question a poser
+
+function verif_question(q){
+
+    let Q = document.getElementById("questionaposer");
+    if(Q == ""){
+
+        filtreQ;
+        // alert("Pose moi une question bro");
+        q.preventDefault();
+    }else{
+        filtreQ;
+        resultat = filtreQ.test(Q.value);
+    }if (resultat == false){
+
+        document.getElementById('11').innerHTML="Troll pas ta question";
+        // alert("Ne troll pas ta question");
+        q.preventDefault();
+    }
+}
+verif_question;
+
+
+//CheckBox
+
+function verif_checkbox(k){
+
+    if(document.getElementById("check").checked == ""){
+
+        document.getElementById('10').innerHTML="Coche ta casse mon chou UWU";
+        // alert("coche ta case mon chou UWU");
+        k.preventDefault();
+    }
+}
+verif_checkbox;
---
+++ Eval/js_Eval.js
@@ -0,0 +1,4 @@
+let bouton_Ex_1 = document.getElementById("Exo_one");
+bouton_Ex_1.addEventListener("click", exercice_1_evaluation_js);
+
+let arrow_Ex_1 = document.getElementById("id_div_ex1");
