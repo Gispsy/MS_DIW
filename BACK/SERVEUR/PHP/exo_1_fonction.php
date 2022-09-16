a8c2d9de02354c310e78117900f3f546f8832a49
Jarod <mordredbta3011@gmail.com>
2022-09-16 12:17:01 +0200

    Fonction_debut

---
+++ BACK/SERVEUR/PHP/exo_1_fonction.php
@@ -0,0 +1,19 @@
+<html>
+
+<!-- Lien a afficher grace a une fonction php  -->
+    <?php 
+        function liens($L){
+
+            $L = htmlentitties("https://www.reddit.com/", "Reddit Hug");
+
+        };
+        liens($L);
+    ?>
+
+
+<!-- Somme de valeur d'un tableau avec fonction php -->
+
+<?php 
+//  $tab = array(4, 3, 8, 2);
+//  $resultat = somme($tab);
+?> 
