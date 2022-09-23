e6d3eb882b5e0207ea1074dcbc99df53a62fd8ec
Jarod <mordredbta3011@gmail.com>
2022-09-23 12:21:12 +0200

    Fin-exo

--- BACK/SERVEUR/PHP/recup_fichier.php
+++ BACK/SERVEUR/PHP/recup_fichier.php
@@ -1,15 +1,24 @@
 <html>
+    <head>
+        <!-- CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    </head>
     <body>
-        <table border = "solid">
-        <?php
-                $fp = file('employe.csv');
-
-                foreach ($fp as $ligne){?>
-            <?php "<tr>"?>
-                    <?php var_dump( explode('NULL',$ligne));?>
-                    <?php echo"<td><th>".$ligne."</th></td><br><br>"; ?>
-            <?php "</tr>"?>
-                    
-
-
-               <?php }?>
+        <table class="table table-dark table-striped">
+            <?php
+                $fp = file('employe.csv');?>
+                <?php foreach($fp as $ligne){?>
+                    <!-- <tr><th> <?= $ligne ?> </th> -->
+                    <tr>
+                <?php  $tab = explode( ',', $ligne );
+                    foreach ($tab as $new){?>   
+                            <td> <?= $new ?> </td>
+                        <?php } ?>
+                    </tr>
+            <?php };?>
+        </table>
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    </body>
+        
+                
